f0648164164e6865841509a86cb7fb5cecf08c45
Michael Hansen <mihansen@microsoft.com>
2018-07-17 21:04:51 -0400

    Added LB endpoint configuration

--- gadgetron-kubernetes-entrypoint.sh
+++ gadgetron-kubernetes-entrypoint.sh
@@ -3,5 +3,11 @@
 #Download the prestop script
 wget -O /opt/gadgetron_kubernetes_prestop.sh https://raw.githubusercontent.com/hansenms/gadgetron-kubernetes/master/gadgetron_kubernetes_prestop.sh
 
-#Run the Gadgetron
-gadgetron
+
+#Get the load balanced endpoint
+gtservice=$(wget -O - -o /dev/null --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --header "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://kubernetes/api/v1/namespaces/default/services/gadgetron-frontend)
+clusterIP=$(echo $gtservice | jq -r .spec.clusterIP)
+clusterPort=$(echo $gtservice | jq -r .spec.ports[0].port)
+
+#Run the Gadgetron using load balanced endpoint
+gadgetron -e ${clusterIP}:${clusterPort}
