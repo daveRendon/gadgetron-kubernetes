938a5df00b53924a6a361f32425436d710ce9ac5
Michael Hansen <mihansen@microsoft.com>
2018-07-17 20:35:40 -0400

    Added entrypoint script

---
+++ gadgetron-kubernetes-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Download the prestop script
+wget -O /opt/gadgetron_kubernetes_prestop.sh https://raw.githubusercontent.com/hansenms/gadgetron-kubernetes/master/gadgetron_kubernetes_prestop.sh
+
+#Run the Gadgetron
+gadgetron
